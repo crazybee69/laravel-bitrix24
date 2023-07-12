60c680d40b8b4c88463da90a5fd7d1c8b14c2586
user <dimochka1994@yandex.ru>
2023-07-12 17:24:31 +0300

    Add business process activity request

---
+++ src/Http/Requests/BusinessProcessActivityRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Crazybee47\Laravel\Bitrix24\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BusinessProcessActivityRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'event_token' => ['required', 'string']
+        ];
+    }
+
+    public function getEventToken(): string
+    {
+        return $this->post('event_token');
+    }
+
+    public function getProperties(): ?array
+    {
+        return $this->post('properties');
+    }
+
+    public function getPropertyValue(string $key): ?string
+    {
+        return \Arr::get($this->getProperties(), $key);
+    }
+}
